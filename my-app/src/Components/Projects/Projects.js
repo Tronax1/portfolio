551c6c5c64a902d306f3768a786feec88bb87f25
Tronax1 <jvilla1714@gmail.com>
2019-02-28 14:06:37 -0500

    Added modals for projects

--- my-app/src/Components/Projects/Projects.js
+++ my-app/src/Components/Projects/Projects.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react'
 
 import "./Projectspage.css"
+import Modal from "./Project_modals/Modal"
 
 export default class Projects extends Component {
+  state = {
+    show: false
+  }
+  showModalBot = () => {
+    this.setState({
+        ...this.state,
+        show: !this.state.show
+    });
+  }
+  showModalMulti = () => {
+    this.setState({
+        ...this.state,
+        show: !this.state.show
+    });
+  }
+  showModalCalc = () => {
+    this.setState({
+        ...this.state,
+        show: !this.state.show
+    });
+  }
+  showModalTicTac = () => {
+    this.setState({
+        ...this.state,
+        show: !this.state.show
+    });
+  }
   render() {
     return (
       <React.Fragment>
-            <div className="ProjectContent">
-                Test page for Projects 
+            <div className = "ProjectContent">
+                <button className = "Projects" onClick={this.showModalBot}>
+                  Discord Bot
+                </button>
+                <Modal onClose={this.showModalBot} show={this.state.show}>
+                  Blah Blah for discord bot
+                </Modal>
+                <button className = "Projects" onClick={this.showModalMulti}>
+                  Array Multiplier
+                </button>
+                <Modal onClose={this.showModalMulti} show={this.state.show}>
+                  Blah Blah for Array Multiplier
+                </Modal>
+                <button className = "Projects" onClick={this.showModalCalc}>
+                  Calculator
+                </button>
+                <Modal onClose={this.showModalCalc} show={this.state.show}>
+                  Blah Blah for calculator
+                </Modal>
+                <button className = "Projects" onClick={this.showModalTicTac}>
+                  Tic-Tac-Toe
+                </button>
+                <Modal onClose={this.showModalTicTac} show={this.state.show}>
+                  Blah Blah for Tic Tac Toe
+                </Modal>
             </div>
         </React.Fragment>
     )
