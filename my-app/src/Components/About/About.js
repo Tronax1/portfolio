38c76db3600e1133c5d41172e5f9d6fe5f6dcfe3
Gongo <jvilla1714@gmail.com>
2019-03-13 14:36:09 -0400

    Fixed dropdown bug and added gradient

--- my-app/src/Components/About/About.js
+++ my-app/src/Components/About/About.js
@@ -7,7 +7,14 @@ export default class About extends Component {
     return (
       <React.Fragment>
             <div className="AboutContent">
-                Test page for about
+                < h1 > About me </h1>
+                < i className = "fas fa-user-tie" > </i>
+                Deserunt occaecat ipsum eu aute laboris sunt duis eiusmod. 
+                Sit nulla enim magna anim consequat. Commodo minim ullamco sit consequat 
+                eu eu aliquip excepteur pariatur laboris cupidatat consectetur nisi commodo. 
+                Id sunt magna ex eu deserunt in irure sit amet ipsum. Fugiat eu eu nostrud 
+                consequat duis esse irure amet eu officia aute. Laborum esse aute excepteur 
+                pariatur consequat consectetur fugiat.
             </div>
         </React.Fragment>
     )
--- my-app/src/Components/Projects/Projects.js
+++ my-app/src/Components/Projects/Projects.js
@@ -8,7 +8,9 @@ export default class Projects extends Component {
       super(props);
       this.state = {
         show: false,
-        show2: false
+        show2: false,
+        show3: false,
+        show4: false
       }
     }
    
@@ -22,18 +24,38 @@ export default class Projects extends Component {
          show2: !this.state.show2
        });
      }
+     showModal3 = () => {
+       this.setState({
+         show3: !this.state.show3
+       });
+     }
+     showModal4 = () => {
+       this.setState({
+         show4: !this.state.show4
+       });
+     }
   render() {
     return (
       <React.Fragment>
-            <div className = "ProjectContent">
-                <button className = "Projects" onClick={this.showModal}>
-                  Discord Bot
-                </button>
-                <Modal onClose={this.showModal} show={this.state.show} info="sdfsdf"/>
-                <button className = "Projects" onClick={this.showModal2}>
-                  Array Multiplier
-                </button>
-                <Modal onClose={this.showModal2} show={this.state.show2} info="Test"/>
+          <div className="Gradient">
+              <div className = "ProjectContent">
+                  <button className = "Projects" onClick={this.showModal}>
+                    Discord Bot
+                  </button>
+                  <button className = "Projects" onClick={this.showModal2}>
+                    Array Multiplier
+                  </button>
+                  <button className = "Projects" onClick={this.showModal3}>
+                    Scientific Calculator
+                  </button>
+                  <button className = "Projects" onClick={this.showModal4}>
+                    Tic-Tac-Toe
+                  </button>
+                  <Modal onClose={this.showModal} show={this.state.show} info="DiscordBot"/>
+                  <Modal onClose={this.showModal2} show={this.state.show2} info="Array Multiplier"/>
+                  <Modal onClose={this.showModal3} show={this.state.show3} info="Scientific Calculator"/>
+                  <Modal onClose={this.showModal4} show={this.state.show4} info="Tic-Tac-Toe"/>
+              </div>
             </div>
         </React.Fragment>
     )
