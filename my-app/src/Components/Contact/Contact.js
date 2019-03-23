aac14c38547323fd41e940efe448fab0668d54ca
Gongo <jvilla1714@gmail.com>
2019-03-23 14:35:52 -0400

    Added animation to navbar

--- my-app/src/Components/Contact/Contact.js
+++ my-app/src/Components/Contact/Contact.js
@@ -7,6 +7,7 @@ export default class Contact extends Component {
   render() {
     return (
       <div>
+        < div className = "transition-b" /> 
             <div className="ContactsContent" >
               <h1>Contact me</h1>
               <Form/>
