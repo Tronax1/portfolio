1586a03d2dee2d8ac2de8f3f4d75fd0648fc7e4b
Gongo <jvilla1714@gmail.com>
2019-05-06 14:47:39 -0400

    Connected front and backend with the contacts page

--- my-app/src/Components/Contact/Form.js
+++ my-app/src/Components/Contact/Form.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import "./Form.css"
 
 export default class Form extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value})
+  }
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    const {name, email, subject, message} = this.state
+
+    const form = await axios.post('/api/form', {
+      name,
+      email,
+      subject,
+      message
+    })
+  }
   render() {
     return (
       <div>
-            <form className="align">
+            <form onSubmit={this.handleSubmit} className="align">
                 <label>
-                 <input type="text" id = "name"className="format" placeholder = "Name"/>
+                 <input type="text" id = "name"className="format" name = "name" 
+                 placeholder = "Name" onChange={this.handleChange}/>
                 </label>
                 <label>
-                 <input type="text" id = "email" className="format" placeholder = "Email"/>
+                 <input type="text" id = "email" className="format" name = "email"
+                 placeholder = "Email" onChange={this.handleChange}/>
                 </label>
                 <label>
-                  <input type="text" id = "subject" className="format" placeholder = "Subject"/>
+                  <input type="text" id = "subject" className="format" name = "subject"
+                  placeholder = "Subject" onChange={this.handleChange}/>
                 </label>
                 <label>
-                  <input type="text" id = "message" className="format message" placeholder= "Message"/>
+                  <input type="textarea" id = "message" className="format message" name = "message" 
+                  placeholder= "Message" onChange={this.handleChange}/>
                 </label>
                 <label>
                   <button id ="submit" className="submitForm">Submit</button>
--- server.js
+++ server.js
@@ -1,28 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const nodeMailer = require('nodemailer')
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-    const customers = [
-        {
-            id: 1,
-            firstName: 'Richard',
-            lastName: 'Telo'
-        },
-        {
-            id: 2,
-            firstName: 'George',
-            lastName: 'Davai'
-        },
-        {
-            id: 3,
-            firstName: 'Joshua',
-            lastName: 'Harlow'
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.post('/api/form', (req, res) => {
+    nodeMailer.createTestAccount((err, account) => {
+        const htmlEmail = `
+            <h3>Contact Details</h3>
+            <ul>
+                <li> Name: ${req.body.name}</li>
+                <li> Email: ${req.body.email}</li>
+            </ul>
+            <h3>Message</h3>
+            <p>${req.body.message}</p>
+        `
+
+        let transporter = nodeMailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'mariane.sawayn@ethereal.email',
+                pass: 'aw8A6VGQfw36Dvxe1Y'
+            }
+        })
+
+        let mailOptions = {
+            from: 'test@testaccount.com',
+            to: 'mariane.sawayn@ethereal.email',
+            replyTo: 'test@testaccount.com',
+            subject: req.body.subject,
+            text: req.body.message,
+            html: htmlEmail
         }
-    ];
-    res.json(customers);
-});
 
-const port = 5000;
+        transporter.sendMail(mailOptions, (err, info) => {
+            if(err){
+                return console.log(err)
+            }
+            console.log('Message sent: %s', info.message)
+            console.log('Message url: %s', nodeMailer.getTestMessageUrl(info))
+        })
+    })
+})
 
-app.listen(port, () => console.log(`Server started running on port ${port}`));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server started running on port ${PORT}`));
