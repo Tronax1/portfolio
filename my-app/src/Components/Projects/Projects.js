f05680dfa90b2ff01cb078b306f55edcdf2cb5d6
Gongo <jvilla1714@gmail.com>
2019-05-15 11:56:50 -0400

    Removed large images and fixed some css errors

--- my-app/src/Components/Projects/Projects.js
+++ my-app/src/Components/Projects/Projects.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react'
 
 import "./Projectspage.css"
 import Modal from "./Project_modals/Modal"
-import Aldebaran from "./Aldebaran.jpg"
+import Aldebaran from "./Aldebaran repo.jpg"
 import Calculator from "./Calculator.jpg"
 import Delay from "../Animation/Delay"
 import ImageBackground from "../Image Backgrounds/ImageBackground"
 import PurpleOcean from '../Images/Purple ocean.jpg'
 import Loading from "../Animation/Loading"
+import Knight from "./Knight move.jpg"
+import TicTacToe from "./Tic Tac Toe.jpg"
 
 export default class Projects extends Component {
     constructor(props){
@@ -54,7 +56,7 @@ export default class Projects extends Component {
                     Aldebaran
                   </button>
                   <button id = "multiplier" className = "Projects" onClick={this.showModal2}>
-                    Array Multiplier
+                    Knight's Move Game
                   </button>
                   <button id = "calculator" className = "Projects" onClick={this.showModal3}>
                     Scientific Calculator
@@ -67,12 +69,15 @@ export default class Projects extends Component {
                     that plays music from a youtube playlist, it also acts like a basic dictionary
                     and displays your osu! statistics."
                     repo = "https://github.com/Tronax1/Aldebaran-" / >
-                  <Modal onClose={this.showModal2} show={this.state.show2} info="Array Multiplier"/>
+                  <Modal onClose={this.showModal2} show={this.state.show2} img={Knight} 
+                  info="This is a player vs AI game
+                  where both players move a chess knight around the board, and whoever runs out of moves looses." 
+                  repo ="https://github.com/Tronax1/Knights-game"/>
                   <Modal onClose={this.showModal3} show={this.state.show3} img={Calculator} 
                   info="This is a scientific calculator that performs the basic arirthmetic operations
                   as well as additional trigonometric functions such as sine, cosine and tangent."
                     repo = "https://github.com/Tronax1/Calculator" / >
-                  <Modal onClose={this.showModal4} show={this.state.show4} info="Simple Tic-Tac-Toe game
+                  <Modal onClose={this.showModal4} show={this.state.show4} img={TicTacToe} info="Simple Tic-Tac-Toe game
                   that allows the player to compete against the computer or against another human."
                     repo = "https://github.com/Tronax1/Tic-Tac-Toe" / >
               </div>
