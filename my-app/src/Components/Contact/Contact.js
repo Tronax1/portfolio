1e4d423c96393cb3309a6066c66d1770263e981a
Gongo <jvilla1714@gmail.com>
2019-05-16 21:08:00 -0400

    Added github link on contact page

--- my-app/src/Components/Contact/Contact.js
+++ my-app/src/Components/Contact/Contact.js
@@ -19,10 +19,16 @@ export default class Contact extends Component {
             <div className="ContactsContent" >
               <h1>Contact me</h1>
               <Form/>
-            < a className="Linkedin" rel="noopener noreferrer"
-            target="_blank" href="https://www.linkedin.com/in/jorgevillarrealgongora/">
-             <i className="fab fa-linkedin fa-4x"></i>
-            </a>
+            <div className="Social-Media">
+              < a  rel="noopener noreferrer"
+                target="_blank" href="https://www.linkedin.com/in/jorgevillarrealgongora/">
+                <i className="fab fa-linkedin fa-4x"></i>
+              </a>
+              < a  rel="noopener noreferrer"
+                target="_blank" href="https://github.com/Tronax1">
+              <i className="fab fa-github fa-4x"></i>
+              </a>
+            </div>
             </div>
       </div>
     )
