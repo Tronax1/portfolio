c9a06db746ca77d2cece1418ddef0697940748e5
Gongo <jvilla1714@gmail.com>
2019-07-23 15:24:20 -0400

    Started changing the image backgrounds

--- my-app/src/Components/Elements/ImageFormat.js
+++ my-app/src/Components/Elements/ImageFormat.js
@@ -1,16 +1,18 @@
 import styled from 'styled-components'
 
 export default styled.div`
+    background-image: url("${props => props.image}");
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: -30;
     background-attachment: fixed;
-    background-repeat: no-repeat;
-    background-size: cover;
-    overflow-y: hidden;
-    @media only screen and (min-width: 768px){
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        z-index: -30;
+        background-repeat: no-repeat;
+        background-size: cover;
+        overflow-y: hidden;
+    @media only screen and (min-width: 768px){  
+        
     }
 `;
--- my-app/src/Components/Image Backgrounds/ImageBackground.js
+++ my-app/src/Components/Image Backgrounds/ImageBackground.js
@@ -4,8 +4,7 @@ import ImageFormat from '../Elements/ImageFormat'
 export default class ImageBackground extends Component {
   render() {
     return (
-      <ImageFormat>
-        <img src={this.props.img} alt=""/>
+      <ImageFormat image={this.props.img}>
       </ImageFormat>
     )
   }
