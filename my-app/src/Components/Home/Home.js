7fbb79ba908c425e38bf0e9fe65aa43f41ff1f6e
Gongo <jvilla1714@gmail.com>
2019-08-12 16:51:41 -0400

    Fixed animation glitch

--- my-app/src/Components/Home/Home.js
+++ my-app/src/Components/Home/Home.js
@@ -7,10 +7,10 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <div className="Home-b">
-                    <p className="Welcome-Message">Hello, I'm Jorge Villarreal</p>
-                    <NavLink className="Project-Redirect" to="/Projects">My Work</NavLink>
-                    <div className="Link-refs">
+                <div className="Home-b" >
+                    <p className="Welcome-Message" id="Initial-animation">Hello, I'm Jorge Villarreal</p>
+                    <NavLink className="Project-Redirect" to="/Projects" id="Initial-animation">My Work</NavLink>
+                    <div className="Link-refs" id="Initial-animation">
                         <a className="Link-icons" href="https://github.com/Tronax1" target="_blank" rel="noopener noreferrer">
                             <i class="fab fa-github fa-4x"></i>
                         </a>
